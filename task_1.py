1a4851c9f5863d5a18c1713cad0808b75e234b1c
Andrei Nadein <andrei.qazah@yandex.ru>
2025-10-04 11:18:33 +0500

    добавить файл проекта третьего спринта

---
+++ task_1.py
@@ -0,0 +1,94 @@
+import datetime
+
+class OnlineSalesRegisterCollector:
+
+    def __init__(self):
+        self.__name_items = []
+        self.__number_items = 0
+        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
+        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+
+    @property
+    def name_items(self):
+        return self.__name_items
+
+    @property
+    def number_items(self):
+        return self.__number_items
+
+    def add_item_to_cheque(self, name):
+        try:
+            if len(name) == 0 or len(name) > 40:
+                raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+            elif name not in self.__item_price:
+                raise NameError(f'Позиция "{name}" отсутствует в товарном справочнике')
+            self.__name_items.append(name)
+            self.__number_items += 1
+        except (ValueError, NameError) as e:
+            print(e)
+
+    def delete_item_from_check(self, name):
+        try:
+            if name not in self.__name_items:
+                raise NameError(f'Позиция "{name}" отсутствует в чеке')
+            self.__name_items.remove(name)
+            self.__number_items -= 1
+        except NameError as e:
+            print(e)
+
+    def check_amount(self):
+        total = []
+        for item in self.__name_items:
+            total.append(self.__item_price[item])
+        if self.__number_items > 10:
+            amount = sum(total) * 0.9
+        else:
+            amount = sum(total)
+        return amount
+
+    def twenty_percent_tax_calculation(self):
+        twenty_percent_tax = []
+        total = []
+        for item in self.__name_items:
+            if self.__tax_rate[item] == 20:
+                twenty_percent_tax.append(item)
+            else:
+                pass
+        for tax20_item in twenty_percent_tax:
+            total.append(self.__item_price[tax20_item])
+        if self.__number_items > 10:
+            tax20_sum = sum(total) * 0.9 * 0.2
+        else:
+            tax20_sum = sum(total) * 0.2
+        return tax20_sum
+
+    def ten_percent_tax_calculation(self):
+        ten_percent_tax = []
+        total = []
+        for item in self.__name_items:
+            if self.__tax_rate[item] == 10:
+                ten_percent_tax.append(item)
+            else:
+                pass
+        for tax10_item in ten_percent_tax:
+            total.append(self.__item_price[tax10_item])
+        if self.__number_items > 10:
+            tax10_sum = sum(total) * 0.9 * 0.1
+        else:
+            tax10_sum = sum(total) * 0.1
+        return tax10_sum
+
+    def total_tax(self):
+        total_tax_sum = self.twenty_percent_tax_calculation() + self.ten_percent_tax_calculation()
+        return total_tax_sum
+
+    @staticmethod
+    def get_telephone_number(telephone_number):
+        try:
+            if not isinstance(telephone_number, int):
+                raise ValueError('Необходимо ввести цифры')
+            elif len(str(telephone_number)) != 10:
+                raise ValueError('Необходимо ввести 10 цифр после "+7"')
+            return f'+7{telephone_number}'
+        except (ValueError, NameError) as e:
+            print(e)
